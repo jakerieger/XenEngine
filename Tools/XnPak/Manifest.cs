937c0b45b4de31e479153fefb8c9325175190be1
Jake Rieger <contact.jakerieger@gmail.com>
2024-10-22 20:03:08 -0400

    remove yaml-cpp, added pugixml

---
+++ Tools/XnPak/Manifest.cs
@@ -0,0 +1,42 @@
+﻿using System.Xml.Linq;
+using System;
+using System.Linq;
+
+namespace XnPak {
+    public struct Asset {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string Build { get; set; }
+
+        public Asset(string name, string type, string build) {
+            this.Name = name;
+            this.Type = type;
+            this.Build = build;
+        }
+
+        public override string ToString() {
+            return $"Name: {Name}, Type: {Type}, Build: {Build}";
+        }
+    }
+
+    public struct Manifest {
+        public string OutputDirectory { get; set; }
+        public bool Compress { get; set; }
+        public Asset[] Assets { get; set; }
+
+        private XDocument _doc;
+
+        public Manifest(string filename) {
+            _doc = XDocument.Load(filename);
+
+            OutputDirectory = _doc.Root.Element("OutputDir").Value;
+            Compress = _doc.Root.Element("Compress").Value.Equals("true", StringComparison.OrdinalIgnoreCase);
+            Assets = _doc.Root.Element("Content").Elements("Asset")
+                .Select(asset => new Asset(
+                    name: asset.Attribute("name").Value,
+                    type: asset.Element("Type").Value,
+                    build: asset.Element("Build").Value))
+                .ToArray();
+        }
+    }
+}
--- Tools/XnPak/Program.cs
+++ Tools/XnPak/Program.cs
@@ -1,9 +1,16 @@
 ﻿using System;
+using System.IO;
 
 namespace XnPak {
   internal abstract class Program {
     public static void Main(string[] args) {
-      Console.WriteLine("Hello, World!");
+      var manifest = new Manifest("../../Test/Content.manifest");
+      Console.WriteLine(manifest.OutputDirectory);
+      Console.WriteLine(manifest.Compress);
+      Console.WriteLine(manifest.Assets.Length);
+      foreach (var asset in manifest.Assets) {
+        Console.WriteLine(asset);
+      }
     }
   }
 }
