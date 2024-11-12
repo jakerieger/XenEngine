dbf91675ff9d88bb19909a50312031df792f0a70
Jake Rieger <contact.jakerieger@gmail.com>
2024-11-11 22:34:51 -0500

    refactoring

--- Tools/XEditor/Source/EditorUI.hpp
+++ Tools/XEditor/Source/EditorUI.hpp
@@ -34,16 +34,6 @@ namespace XEditor {
             // Load default theme
             EditorStyle::LoadAndApplyStyle("XenDark.xml");
 
-            // ImGui::StyleColorsDark();
-
-            // When viewports are enabled we tweak WindowRounding/WindowBg so platform windows can
-            // look identical to regular ones.
-            // ImGuiStyle& style = ImGui::GetStyle();
-            // if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable) {
-            //     style.WindowRounding              = 0.0f;
-            //     style.Colors[ImGuiCol_WindowBg].w = 1.0f;
-            // }
-
             ImGui_ImplGlfw_InitForOpenGL(*window, true);
             ImGui_ImplOpenGL3_Init("#version 130");
         }
