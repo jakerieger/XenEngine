7380a23551a38e153ad335e728c7e1c5885ee32d
Jake Rieger <contact.jakerieger@gmail.com>
2024-11-16 04:02:59 -0500

    pong app

--- Examples/Pong/Source/Pong.cpp
+++ Examples/Pong/Source/Pong.cpp
@@ -1,3 +1,20 @@
+// Author: Jake Rieger
+// Created: 11/14/2024.
+//
+
+#include <Types/Types.h>
+#include <OpenGLTK/Include/Glfw/GlfwApp.h>
+
+class Pong final : public GLTK::IGlfwApp {
+public:
+    Pong(int width, int height) : IGlfwApp("Pong", width, height) {}
+    void CreateGLResources() override;
+    void DestroyGLResources() override;
+    void Update() override;
+    void Render() override;
+};
+
 int main() {
-    return 0;
+    Pong app(1280, 720);
+    app.Run();
 }
