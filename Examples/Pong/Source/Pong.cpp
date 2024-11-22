318e6027769d0a50880f799c60c78566af2ce988
Jake Rieger <contact.jakerieger@gmail.com>
2024-11-21 19:51:19 -0500

    component adding logic appears broken

--- Examples/Pong/Source/Pong.cpp
+++ Examples/Pong/Source/Pong.cpp
@@ -2,6 +2,8 @@
 // Created: 11/14/2024.
 //
 
+#include "Scene.hpp"
+
 #include <Expect.hpp>
 #include <Texture.hpp>
 #include <ContentManager.hpp>
@@ -14,29 +16,22 @@ public:
     Pong() : IGame("Pong") {}
 
     void LoadContent() override {
-        auto result     = mContentManager->LoadAsset("sprites/ball");
-        const auto ball = Expect(result, "Failed to load ball asset");
-        mBallTexture    = CAST<i32>(Texture::LoadFromMemory(ball.Data, 48, 48));
-
-        result            = mContentManager->LoadAsset("sprites/paddle");
-        const auto paddle = Expect(result, "Failed to load paddle asset");
-        mPaddleTexture    = CAST<i32>(Texture::LoadFromMemory(paddle.Data, 24, 200));
+        mMainScene = Scene::Load("Scenes/Main.xscene");
     }
 
     void UnloadContent() override {
-        Texture::Delete(mBallTexture);
-        Texture::Delete(mPaddleTexture);
+
     }
 
     void Update(const Weak<Clock>& clock) override {
         const auto dT = clock.lock()->GetDeltaTime();
+        // mMainScene->Update({}, dT);
     }
 
     void Draw() override {}
 
 private:
-    i32 mBallTexture   = -1;
-    i32 mPaddleTexture = -1;
+    Unique<Scene> mMainScene;
 };
 
 int main() {
